2e8981aa8339ea5d841ed3f9cbce4d11634dfd30
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-10-13 00:46:35 -0400

    wrote cows_controller CRUD

--- app/controllers/cows_controller.rb
+++ app/controllers/cows_controller.rb
@@ -1,3 +1,70 @@
 class CowsController < Sinatra::Base
-  
+     get "/cows" do
+        if is_logged_in? 
+            @cows = Cow.all
+            erb :'/index'
+        else
+            redirect to '/login'
+        end
+    end
+
+    get "/cows/new" do
+        if is_logged_in?  
+           erb :'/cows/new'
+        else
+           redirect to '/login'
+        end
+    end
+
+    post "/cows" do
+        if is_logged_in?
+          if params[:content] == ""
+            redirect to "/cows/new"
+          else
+            @cow = current_user.cows.build(params)
+            if @cow.save
+              redirect to "/cows/#{@cow.id}"
+            else
+              redirect to "/cows/new"
+            end
+          end
+        else
+          redirect to '/login'
+        end
+      end
+
+    get "/cows/:id" do
+        if is_logged_in?
+            @cow = Cows.find_by_id(params[:id])
+            erb :'/cows/show'
+        else
+            redirect to '/login'
+        end
+    end
+
+    get "/cows/:id/edit" do
+            if is_logged_in? && @cow && @cow.user == current_user
+                @cow = Tweet.find_by_id(params[:id])
+                
+                erb :"/cows/#{@cow.id}"
+            else
+                redirect to '/login'
+        end
+    end
+ 
+
+    delete "/cows/:id/delete" do
+        if is_logged_in?
+          @cow = Tweet.find_by_id(params[:id])
+          if @cow && @cow.user == current_user
+            @cow.delete
+          end
+          redirect to '/cows'
+        else
+          redirect to '/login'
+        end
+    end
+
+end
+ 
 end
