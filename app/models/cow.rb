c51bb34e058d07c2bdda64f3073b66c88f1bbbc5
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-10-13 01:35:48 -0400

    migration associations in models

--- app/models/cow.rb
+++ app/models/cow.rb
@@ -2,5 +2,12 @@ class  Cow < ActiveRecord::Base
     
     belongs_to :user
 
+    def slug
+        username.downcase.gsub(" ","-")
+    end
+    
+    def self.find_by_slug(slug)
+        User.all.find{|user| user.slug == slug}
+    end
 
 end
--- app/models/field.rb
+++ app/models/field.rb
@@ -1,6 +1,5 @@
 class  Field < ActiveRecord::Base
     
     belongs_to :user
-    has_many :cows
     
 end
